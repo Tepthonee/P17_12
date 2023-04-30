ea6a6f983e2dbd7d397eb213d192a5bb6396caf1
ArabSourceBest <129210989+TepthonArab@users.noreply.github.com>
2023-04-30 09:05:53 +0300

    Add files via upload

---
+++ tepthon/assistant/__init__.py
@@ -0,0 +1,4 @@
+from tepthon import BOTLOG, BOTLOG_CHATID, tepthon
+
+from ..Config import Config
+from ..core.inlinebot import *
---
+++ tepthon/assistant/hide.py
@@ -0,0 +1,21 @@
+import json
+import os
+import re
+
+from telethon.events import CallbackQuery
+
+from tepthon import tepthon
+
+
+@tepthon.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
+async def on_plug_in_callback_query_handler(event):
+    timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
+    if os.path.exists("./tepthon/hide.txt"):
+        jsondata = json.load(open("./tepthon/hide.txt"))
+        try:
+            reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
+        except KeyError:
+            reply_pop_up_alert = "هذه الرسالة غير متوفرة الان في سيرفرات تيبثون"
+    else:
+        reply_pop_up_alert = "هذه الرسالة غير متوفرة"
+    await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
