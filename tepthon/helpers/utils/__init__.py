6f9f60f387e8fc76734f5449b6cf4a626db8c036
ArabSourceBest <129210989+TepthonArab@users.noreply.github.com>
2023-04-30 11:30:31 +0300

    Update __init__.py

--- tepthon/helpers/utils/__init__.py
+++ tepthon/helpers/utils/__init__.py
@@ -1,5 +1,19 @@
-from ..helpers.progress import *
-from .checks import *
-from .decorators import *
-from .pluginmanager import *
-from .startup import *
+from .extdl import *
+from .paste import *
+
+flag = True
+check = 0
+while flag:
+try:
+  from . import format as _format
+  from . import utils as _jmthonutils
+  from .events import *
+  from .format import *
+  from .utils import *
+  
+  break
+  except ModuleNotFoundError as e:
+    install_pip(e.name)
+    check += 1
+    if check > 5:
+      break
