aef8e164777854a375d7db019e0b4910d4c6e0a3
روجر <104784913+rogerpq@users.noreply.github.com>
2023-05-15 20:34:40 +0300

    Update __main__.py

--- tepthon/__main__.py
+++ tepthon/__main__.py
@@ -1,14 +1,14 @@
+import contextlib
 import sys
 
-from aiohttp import web
-
 import tepthon
-from tepthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID, tbot
+from tepthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+from razan.strings import blacklisted_users
 
 from .Config import Config
 from .core.logger import logging
-from .core.server import web_server
 from .core.session import tepthon
+from .sql_helper.globals import gvarstatus
 from .utils import (
     add_bot_to_logger_group,
     load_plugins,
@@ -23,66 +23,67 @@ LOGS = logging.getLogger("سورس تيبثون")
 
 cmdhr = Config.COMMAND_HAND_LER
 
+try:
+    LOGS.info("يتم بدء البوت المساعد")
+    jmbot.loop.run_until_complete(setup_bot())
+    LOGS.info("اكتملت عمليه البوت المساعد")
+except Exception as e:
+    LOGS.error(f"{e}")
+    sys.exit()
 
-async def jmthons(session=None, client=None, session_name="Main"):
-    if session:
-        LOGS.info(f"••• جار بدأ الجلسة [{session_name}] •••")
+try:
+    LOGS.info("يتم تفعيل وضع حمايه الحساب من الاختراق")
+    tepthon.loop.create_task(saves())
+    LOGS.info("تم تفعيل وضع حمايه الحساب من الاختراق")
+except Exception as bb:
+    LOGS.error(f"- {bb}")
+    sys.exit()
+
+
+try:
+    LOGS.info("يتم تفعيل وضع الانلاين")
+    tepthon.loop.run_until_complete(mybot())
+    LOGS.info("تم تفعيل وضع الانلاين بنجاح ✓")
+except Exception as meo:
+    LOGS.error(f"- {meo}")
+    sys.exit()
+
+
+async def startup_process():
+    if tepthon.uid in blacklisted_users:
+        LOGS.info("انت لا يمكنك تنصيب سورس تيبثون عزيزي دي")
+        return
+    if not gvarstatus("TNSEEB"):
         try:
-            await client.start()
-            return 1
-        except:
-            LOGS.error(f"خطأ في الجلسة {session_name}!! تأكد وحاول مجددا !")
-            return 0
+            await verifyLoggerGroup()
+            await load_plugins("plugins")
+            await load_plugins("assistant")
+            LOGS.info("============================================================")
+            LOGS.info("تم انتهاء عملية التنصيب بنجاح")
+            LOGS.info(
+                f"لمعرفة اوامر السورس ارسل {cmdhr}الاوامر\
+                \nمجموعة قناة السورس  https://t.me/Tepthon_help"
+            )
+            LOGS.info("============================================================")
+            await verifyLoggerGroup()
+            await add_bot_to_logger_group(BOTLOG_CHATID)
+            if PM_LOGGER_GROUP_ID != -100:
+                await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+            await startupmessage()
+            return
+        except Exception as e:
+            LOGS.info(str(e))
+            return
     else:
-        return 0
+        LOGS.info("انت لا يمكنك تنصيب سورس تيبثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس تيبثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس تيبثون عزيزي دي")
 
 
-# تأكد من تنصيب بعض الاكواد
-async def jmthonstart(total):
-    await setup_bot()
-    await mybot()
-    await verifyLoggerGroup()
-    await add_bot_to_logger_group(BOTLOG_CHATID)
-    if PM_LOGGER_GROUP_ID != -100:
-        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
-    await startupmessage()
-    await saves()
+tepthon.loop.run_until_complete(startup_process())
 
-
-async def start_jmthon():
-    try:
-        tbot_id = await tbot.get_me()
-        Config.TG_BOT_USERNAME = f"@{tbot_id.username}"
-        tepthon.tgbot = tbot
-        LOGS.info("•••  جار بدا سورس تيبثون •••")
-        CLIENTR = await jmthons(Config.STRING_SESSION, tepthon, "STRING_SESSION")
-        await tbot.start()
-        total = CLIENTR
-        await load_plugins("plugins")
-        await load_plugins("assistant")
-        LOGS.info(f"تم انتهاء عملية التنصيب بنجاح")
-        LOGS.info(
-            f"لمعرفة اوامر السورس ارسل {cmdhr}الاوامر\
-        \nمجموعة قناة السورس  https://t.me/Tepthon_Help"
-        )
-        LOGS.info(f"» عدد جلسات التنصيب الحالية = {str(total)} «")
-        await jmthonstart(total)
-        app = web.AppRunner(await web_server())
-        await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, Config.PORT).start()
-    except Exception as e:
-        LOGS.error(f"{str(e)}")
-        sys.exit()
-
-
-tepthon.loop.run_until_complete(start_jmthon())
-
-if len(sys.argv) not in (1, 3, 4):
-    tepthon.disconnect()
-else:
-    try:
+if len(sys.argv) in {1, 3, 4}:
+    with contextlib.suppress(ConnectionError):
         tepthon.run_until_disconnected()
-    except ConnectionError:
-        pass
-        
+else:
+    tepthon.disconnect()
